482538de19527450822aa7b7b04e96de93ebed88
Krish C Naik <krishnaik06@gmail.com>
2021-01-13 18:02:18 +0530

    Add files via upload

--- sample_blockchain.py
+++ sample_blockchain.py
@@ -12,20 +12,25 @@ def mine(block_number, transactions, previous_hash, prefix_zeros):
         text = str(block_number) + transactions + previous_hash + str(nonce)
         new_hash = SHA256(text)
         if new_hash.startswith(prefix_str):
-            print(f"Yay! Successfully mined bitcoins with nonce value:{nonce}")
+            print("We have successfully mined our block and the nonce is {}".format(nonce))
             return new_hash
 
-    raise BaseException(f"Couldn't find correct has after trying {MAX_NONCE} times")
+    raise BaseException("We did not find the hash for {} iterations".format(MAX_NONCE))
 
 if __name__=='__main__':
     transactions='''
-    Krish-tranaction-50
+    Krish-tranaction-50$
     '''
-    difficulty=3 # try changing this to higher number and you will see it will take more time for mining as difficulty increases
+    prefix_zeros=6 # try changing this to higher number and you will see it will take more time for mining as difficulty increases
     import time
     start = time.time()
     print("start mining")
-    new_hash = mine(2,transactions,'000000067f29319ce46e3d4611481b8d826d9519242f675d67ff6c9c987afcc3', difficulty)
+    new_hash = mine(2,transactions,'000000067f29319ce46e3d4611481b8d826d9519242f675d67ff6c9c987afcc3', prefix_zeros)
     total_time = str((time.time() - start))
-    print(f"end mining. Total time Taken by the mining is: {total_time} seconds")
+    print("end mining. Total time Taken by the mining is: {} seconds".format(total_time))
     print(new_hash)
+    
+
+    
+    
+    
